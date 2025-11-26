4b971140959f98f6f71f8eec843fb36efd4ec7c0
Paudel3101 <bishalsharma24112002@gmail.com>
2025-11-26 18:06:05 -0500

    Initial Commit

---
+++ WellnessHub/WellnessHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  WellnessHubApp.swift
+//  WellnessHub
+//
+//  Created by Bishal Paudel on 11/26/25.
+//
+
+import SwiftUI
+
+@main
+struct WellnessHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
